269caca3535cb90313573bd2478e348e10e2781e
DKay-code <derya.kaygisiz@outlook.com>
2021-04-24 00:02:19 +0300

    commit tree c435b0ae6dace078e0bda217036c18b7f355c2ea

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 let newArray=[];
 function writeCards(name, event) {
   for (let i = 0; i < name.length; i++) {
-    // console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
     newArray[i] = "Thank you, " + name[i] + ", for the wonderful " + event + " gift!";
   } 
 
